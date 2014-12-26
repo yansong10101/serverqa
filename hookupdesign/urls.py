f5161bd1637e9831cc5b74f211d8871ec5c29b39
yansong10101@gmail.com <yansong10101@gmail.com>
2014-12-25 23:48:27 -0800

    1

--- hookupdesign/urls.py
+++ hookupdesign/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'hookupdesign.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^index/', include('designweb.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
