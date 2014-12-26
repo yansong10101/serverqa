45c7e61e720f068109cbff941843feeb39442215
yansong10101@gmail.com <yansong10101@gmail.com>
2014-12-25 22:49:28 -0800

    1

--- designweb/urls.py
+++ designweb/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from hookupdesign import views
+from designweb import views
 
 urlpatterns = {
   url(r'^$', views.index, name='index'),
--- designweb/views.py
+++ designweb/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Hello, this is hook-up design website...")
+    return HttpResponse("Hello, this is hook-up design website...")
