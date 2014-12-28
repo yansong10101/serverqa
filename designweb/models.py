ff8d119d31abf0b940a0cb39e9f7617b366ef65e
yansong10101@gmail.com <yansong10101@gmail.com>
2014-12-28 01:51:09 -0800

    1

--- designweb/models.py
+++ designweb/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class test_db(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    user_name = models.CharField(max_length=50)
+    user_address = models.CharField(max_length=50)
+    city = models.CharField(max_length=10)
+    state = models.CharField(max_length=2)
