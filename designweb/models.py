a9fa7a90baf6bf42d19fc3f6cc65dab851cb98d3
yansong10101@gmail.com <yansong10101@gmail.com>
2015-01-03 01:05:44 -0800

    1

--- designweb/models.py
+++ designweb/models.py
@@ -14,9 +14,30 @@ class test_db(models.Model):
     user_address = models.CharField(max_length=50)
     city = models.CharField(max_length=10)
     state = models.CharField(max_length=2)
+    zip = models.IntegerField()
 
     objects = models.Manager()
     cust_objects = test_db_query.as_manager()
 
     def __unicode__(self):
         return self.user_name
+
+
+# User profile
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(User)
+#     is_designer = models.BooleanField(default=False)
+#     designer_type = models.CharField(max_length=50, blank=True)
+#     address1 = models.CharField(max_length=50, blank=True)
+#     address2 = models.CharField(max_length=50, blank=True)
+#     city = models.CharField(max_length=25, blank=True)
+#     state = models.CharField(max_length=2, blank=True)
+#     zip = models.CharField(max_length=8, blank=True)
+#
+#     def raw_data(self):
+#         return {
+#             'user': self.user.id,
+#         }
+#
+#     def __unicode__(self):
+#         return self.user.id
--- hookupdesign/urls.py
+++ hookupdesign/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'index/', include('designweb.urls', namespace='design')),
+                       url(r'', include('designweb.urls', namespace='design')),
                        # url(r'^index/', include('designweb.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        )
