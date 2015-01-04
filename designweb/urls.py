b2eb07a91bca50462b8af7e99ff2d6fe8f1340d7
yansong10101@gmail.com <yansong10101@gmail.com>
2015-01-04 12:24:52 -0800

    1

--- designweb/urls.py
+++ designweb/urls.py
@@ -3,4 +3,6 @@ from designweb import views
 
 urlpatterns = {
     url(r'^index/', views.index, name='index'),
+    url(r'logout/', views.logout_view, name='logout'),
+    url(r'login/', views.login_view, name='login'),
 }
--- designweb/views.py
+++ designweb/views.py
@@ -9,10 +9,10 @@ from designweb import tests
 
 
 # Create your views here.
-@ensure_csrf_cookie
+# @ensure_csrf_cookie
 def index(request):
-    if request.user is not None:
-        user = request.user
+    user = request.user
+    if not user.is_anonymous():
         return HttpResponse("Hello, " + user.username)
     else:
         user = tests.db_read()
@@ -30,25 +30,28 @@ def signup(request):
             login(request, user)
             return redirect(reverse(index))     # need redirect to successful page, or to home page and show msg
         else:
-            render(request, 'signup.html', {'form': form})
-    else:   # To Do - do we need specify 'GET' method here?
-        pass
+            return render(request, 'signup.html', {'form': form})
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
 
 
-def login(request):
+def login_view(request):
     if request.method == 'POST':
-        username = request['username']
-        password = request['password']
+        username = request.POST['username']
+        password = request.POST['password']
+        print(username + ' ' + password)
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect(reverse(index))     # need redirect to membership home page
         else:
-            render(request, 'login.html')
-    else:   # To Do - do we need specify 'GET' method here?
-        pass
+            return render(request, 'login.html')
+    else:
+        print('test point...')
+        return render(request, 'login.html')
 
 
-def logout(request):
+def logout_view(request):
     logout(request)
     return redirect(reverse(index))
